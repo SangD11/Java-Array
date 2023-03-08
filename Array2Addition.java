22a564325a474b6616df930aca74820ddea5a2a9
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:47:16 -0500

    Add files via upload

---
+++ Array2Addition.java
@@ -0,0 +1,23 @@
+package javaArray.javaArray;
+
+public class Array2Addition {
+    public static void main(String[] args) {
+        int myVar[] = {5,6,8,9,11};
+           int result = 0;
+        for (int i=0;i<=4;i++ ) {
+            result = result + myVar[i];
+        }
+            System.out.println(result);
+
+        // int myvar [0] = 5;
+        // int myvar [1] = 6;
+        //int myvar [2] = 8;
+        //int myvar [3] = 9;
+        //int myvar [4] = 11;
+        // int myVar[]= {5,6,8,9,11};
+        //int result = myVar[0]+myVar[1]+myVar[2]+myVar[3]+myVar[4];
+        //System.out.println(result);
+
+
+    }
+}

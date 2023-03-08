e254b2a9519c44ef264c646e090d2bfd52c23f86
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:46:54 -0500

    Add files via upload

---
+++ Array1Indexing.java
@@ -0,0 +1,19 @@
+package javaArray.javaArray;
+
+public class Array1Indexing {
+    public static void main(String[] args) {
+       int myVar[]= {5,6,8,9,11};
+       for (int i = 0; i <=4;i++){
+           System.out.println(myVar[i]);
+        }
+
+
+
+
+
+
+    }
+
+
+
+}

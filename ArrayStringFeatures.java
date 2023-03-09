20fdd670318f9964372f0681bc19967d39a02df0
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 20:03:16 -0500

    Add files via upload

---
+++ ArrayStringFeatures.java
@@ -0,0 +1,14 @@
+package javaArray.javaArray;
+
+public class ArrayStringFeatures {
+    public static void main(String[] args) {
+       String myVar = "My name is Bobby";
+      // System.out.println(myVar);
+      //  String myVar = "My name is Bobby";
+        //System.out.println(myVar.toUpperCase());
+        //System.out.println(myVar.toLowerCase());
+        //System.out.println(myVar.length());
+        //System.out.println(myVar.charAt(6));
+        System.out.println(myVar.indexOf("m"));
+    }
+}

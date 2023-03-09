1df780275c14c6f7a7b141afc0563d67b5dead15
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 20:12:56 -0500

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,25 @@
+package javaArray.javaArray;
+
+public class ArrayString {
+    public static void main(String[] args) {
+       // String is a datatype falls under data type not a data type.String itself is a class.
+        // Strings comes with other benefits
+
+         //this is my Array practice, doing my homework,learning java.
+       String myList[] ={"this is my Array practice", "doing my homework","learning java"};
+      // for (int i = 0; i <myList.length;i++){
+          // System.out.println(myList[i]);
+          String myVar= "my name is Jack";
+           System.out.println(myVar.length());
+
+          // String myList[] ={"this is my Array practice", "doing my homework","learning java"};
+           // for (int i = 0; i <myList.length-1;i++){
+       // System.out.println(myList[i]);
+
+
+
+
+
+
+            }
+}

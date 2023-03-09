d545acd04ebaafd4e265be7e80da8a3e37c1c22c
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 20:02:56 -0500

    Add files via upload

---
+++ Array3length.java
@@ -0,0 +1,20 @@
+package javaArray.javaArray;
+
+public class Array3length {
+    public static void main(String[] args) {
+        //MyVar.length gives you length of the index.Java gives you the length you don't need to count.
+
+        int myVar[] = {5, 6, 4, 7, 4, 11, 10, 21, 23, 24,20,32,36,40,50};
+       // System.out.println(myVar.length);
+        // To find the position of the value.
+           // System.out.println(myVar[10]);
+
+
+
+
+
+       // for (int i = 0; i <= myVar.length - 1; i++) {
+        //    System.out.println(myVar[i]);
+      //  }
+    }
+}
